abae33cd7fbaaae7e4f53f4c5c85942835b3aec8
ttomczak3 <ttomczak1512@protonmail.com>
2022-04-02 21:44:04 -0500

    Updated for Azure

--- GamingQuiz/Program.cs
+++ GamingQuiz/Program.cs
@@ -17,11 +17,12 @@ builder.Services.AddSwaggerGen();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+app.UseSwagger();
+app.UseSwaggerUI( c  => 
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Employee API V1");
+    c.RoutePrefix = string.Empty;
+});
 
 app.UseHttpsRedirection();
 
